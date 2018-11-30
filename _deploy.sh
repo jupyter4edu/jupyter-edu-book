53bf252f6615be374095e2c2def4fb28d34a6773
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2018-11-30 12:44:43 -0500

    Working on setting up Travis deploy

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "kyle.niemeyer@gmail.com"
+git config --global user.name "Kyle Niemeyer"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
