0d6ff21d9cb0e1dbb0b2c5ea22a02fe3e1232588
Douglas Blank <doug.blank@gmail.com>
2018-12-05 20:23:47 -0500

    Manual copy of icons to gh-pages

--- _deploy.sh
+++ _deploy.sh
@@ -11,6 +11,12 @@ git config --global user.name "Kyle Niemeyer"
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
+## Manual copies to get the icons:
+cp ../images/caution.png ./images
+cp ../images/important.png ./images
+cp ../images/note.png ./images
+cp ../images/tip.png ./images
+cp ../images/warning.png ./images
 git add --all *
 git commit -m"Update the book" || true
 git push -q origin gh-pages
